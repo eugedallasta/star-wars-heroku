3917ad3c4a21c45fb0d55ce49bed4077e1ea64e7
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-05-27 10:43:28 -0300

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCwKZRmIRL9LKw0vdFTXuPdp6GBNnj5MUE",
+    authDomain: "angular-auth-2f066.firebaseapp.com",
+    projectId: "angular-auth-2f066",
+    storageBucket: "angular-auth-2f066.appspot.com",
+    messagingSenderId: "314245284519",
+    appId: "1:314245284519:web:840c16765105331ece1182",
+    measurementId: "G-LFTCGNWCXF"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
 
   firebaseConfig: {
     apiKey: "AIzaSyCwKZRmIRL9LKw0vdFTXuPdp6GBNnj5MUE",
